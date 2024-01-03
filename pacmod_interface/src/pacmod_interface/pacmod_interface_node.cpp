3d60e21a83c86f01c62d3699afd152b287ba83c0
sykwer <sykwer@gmail.com>
2024-01-04 08:55:05 +0900

    autoware-on-linux xx1 ver-20240104

--- pacmod_interface/src/pacmod_interface/pacmod_interface_node.cpp
+++ pacmod_interface/src/pacmod_interface/pacmod_interface_node.cpp
@@ -17,11 +17,17 @@
 
 #include <memory>
 
+#include "static_callback_isolated_executor.hpp"
+
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
   auto node = std::make_shared<PacmodInterface>();
-  rclcpp::spin(node);
+
+  auto executor = std::make_shared<StaticCallbackIsolatedExecutor>();
+  executor->add_node(node);
+  executor->spin();
+
   rclcpp::shutdown();
   return 0;
 }
